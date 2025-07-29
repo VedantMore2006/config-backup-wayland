eacd2458c30bc98a29e888f5fe16ad38c42441b1
VedantMore2006 <vedantmoremain@gmail.com>
2025-07-29 14:46:37 +0530

    readme

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,13 +2,19 @@
 require("config.lazy")
 
 -- Load custom folder scripts
-local custom_path = vim.fn.stdpath('config') .. '/lua/custom'
-package.path = package.path .. ';' .. custom_path .. '/?.lua'
+local custom_path = vim.fn.stdpath("config") .. "/lua/custom"
+package.path = package.path .. ";" .. custom_path .. "/?.lua"
 
 -- Require output.lua (or image_display.lua if you named it that)
-pcall(require, 'custom.output')
+pcall(require, "custom.output")
 
-for _, file in ipairs(vim.fn.glob(custom_path .. '/*.lua', true, true)) do
-    local module = file:match("^.+/(.+)%.lua$")
-    pcall(require, 'custom.' .. module)
-    end
+for _, file in ipairs(vim.fn.glob(custom_path .. "/*.lua", true, true)) do
+  local module = file:match("^.+/(.+)%.lua$")
+  pcall(require, "custom." .. module)
+end
+--#region
+--
+--
+--
+--
+--
